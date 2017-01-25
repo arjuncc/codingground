9281583b7c1932f281db0436df5fa4276168f5bb
arjuncc <999arjun@gmail.com>
2017-01-25 18:17:22 +0530

    Created by Coding Ground Client

---
+++ New Project-20170124/TestDummyCode.java
@@ -0,0 +1,20 @@
+/***
+ * Class : User TestCode
+ * Required : Basic idea
+ * Req. Knowledge : coding conventions, Abstraction
+ * Complexity : min
+ * Version : 1.0.0
+ */
+import com.java.*;
+
+public class TestDummyCode{
+    
+     public static void main(String []args){
+        User u = new User();
+        u.userName = "firstUser";
+        System.out.println("Is user valid : "+u.valid());
+        u.userName = "SecondUserWithMoreData";
+        System.out.println("Is user valid : "+u.valid());
+        
+     }
+}
